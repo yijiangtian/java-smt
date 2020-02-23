d391a6d8c8228e67ab54deac65b0c2620c3ddf2d
Radu <radu.rusanu@gmail.com>
2020-02-23 16:46:25 +0100

    initial commit

---
+++ src/org/sosy_lab/java_smt/test/IntervalOptimizerTest.java
@@ -0,0 +1,37 @@
+/*
+ *  JavaSMT is an API wrapper for a collection of SMT solvers.
+ *  This file is part of JavaSMT.
+ *
+ *  Copyright (C) 2007-2016  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.sosy_lab.java_smt.test;
+import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.java_smt.api.ProverEnvironment;
+import org.sosy_lab.java_smt.SolverContextFactory;
+import org.sosy_lab.java_smt.api.SolverContext;
+import org.sosy_lab.java_smt.api.SolverContext.ProverOptions;
+
+public class IntervalOptimizerTest {
+
+  public static void main(String[] args) throws InvalidConfigurationException {
+
+    ProverEnvironmentSubjectTest test = new ProverEnvironmentSubjectTest();
+    test.setupFormulas();
+    SolverContext context = SolverContextFactory.createSolverContext(test.solverToUse());
+    ProverEnvironment env = context.newProverEnvironment(ProverOptions.GENERATE_MODELS);
+  }
+}
